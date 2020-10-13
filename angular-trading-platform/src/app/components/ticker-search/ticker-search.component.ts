68230704045912a181fec405926fbc66419559bd
Shamsuddin Rehmani <rehmani@usc.edu>
2020-10-12 23:54:32 -0400

    starting from angular(client) app. Setup the basic structure and the navbar. Next add reactive form and use material autocomplete to implement the search input for ticker

---
+++ angular-trading-platform/src/app/components/ticker-search/ticker-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ticker-search',
+  templateUrl: './ticker-search.component.html',
+  styleUrls: ['./ticker-search.component.css']
+})
+export class TickerSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
